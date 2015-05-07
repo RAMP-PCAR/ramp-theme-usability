9ce66688a3448fd3026842f6fe34aece3a1619eb
Jack <Jack@>
2015-05-07 16:08:39 -0400

    fix(map-export-ui): fix map export title in full screen mode
    export map title gets shift up by full screen mode code. Modify theme.js to use more specific tag filter
    
    closes #9581

--- src/js/RAMP/Modules/theme.js
+++ src/js/RAMP/Modules/theme.js
@@ -28,7 +28,7 @@ define(["dojo/_base/lang", "utils/util"],
             navigation = $("#wb-bar"),
             title = navigation.next(),
 
-            header = $("header"),
+            header = $("body>header"),
 
             transitionDuration = 0.5,
 
